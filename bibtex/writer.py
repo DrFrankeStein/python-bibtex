1aba58ca90bbed56707d70ff912dcce15166e243
Lars Franke <lars.ch.franke@gmail.com>
2017-01-23 16:56:21 +0100

    Add basic writer functions

--- bibtex/writer.py
+++ bibtex/writer.py
@@ -1,7 +1,21 @@
-import re
+def cite_str(bibitem):
+    # TODO: check for maleformed items
+    head = '@{item[type]}{{{item[id]},\n\t'.format(item=bibitem)
+    key_value_pairs = ['{} = "{}"'.format(key, val)
+                       for key, val in bibitem.items()
+                       if key not in ('id','type')]
+    return  head + ',\n\t'.join(key_value_pairs) + '\n}'
+
+
+def write(bib):
+    for item in bib:
+        yield cite_str(item)
+
 
 def dumps(bib):
-    pass
+    return '\n'.join(list(write(bib)))
 
-def dump(bib):
-    pass
+
+def dump(bib, filelike):
+    for item in write(bib):
+        filelike.write(item)

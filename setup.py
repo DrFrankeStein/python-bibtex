3481faa634145515ab77c6dd437e3146e594e6dd
Lars Franke <lars.franke@mailbox.org>
2017-03-30 22:39:04 +0200

    Fix license

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(
     url='https://github.com/DrFrankeStein/python-bibtex',
     author='Lars Franke',
     author_email='lars.ch.franke@gmail.com',
-    license='GPLv3',
+    license='LGPLv3',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Topic :: Text Processing :: Markup :: LaTeX',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
     ] + ['Programming Language :: Python :: {v}'.format(v=x) for x in
          '2 2.6 2.7 3 3.3 3.4 3.5 3.6'.split()],
     keywords='bibtex latex',
